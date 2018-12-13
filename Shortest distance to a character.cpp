baf3de6d3dccc2b88b300ff839014071aceffcf3
ajayguna-96 <ajayguna.96@gmail.com>
2018-12-12 21:02:34 -0700

    Update Shortest distance to a character.cpp

--- Shortest distance to a character.cpp
+++ Shortest distance to a character.cpp
@@ -18,44 +18,4 @@ public:
         return dist;
 
     }
-};class Solution {
-public:
-    vector<int> shortestToChar(string S, char C) {
-        vector<int> C_index;
-        vector<int> dist(S.length(),INT_MAX);
-        for(int i = 0;i<S.length();i++){
-            if(S[i] == C){
-                C_index.push_back(i);
-            }
-        }
-        for(int i = 0; i<S.length(); i++){
-            for(int j =class Solution {
-public:
-    vector<int> shortestToChar(string S, char C) {
-        vector<int> C_index;
-        vector<int> dist(S.length(),INT_MAX);
-        for(int i = 0;i<S.length();i++){
-            if(S[i] == C){
-                C_index.push_back(i);
-            }
-        }
-        for(int i = 0; i<S.length(); i++){
-            for(int j = 0; j<C_index.size() ;j++){
-                if(dist[i] >= abs(i-C_index[j])){
-                    dist[i] = abs(i-C_index[j]);
-                }
-            }
-        }
-        return dist;
-
-    }
-}; 0; j<C_index.size() ;j++){
-                if(dist[i] >= abs(i-C_index[j])){
-                    dist[i] = abs(i-C_index[j]);
-                }
-            }
-        }
-        return dist;
-
-    }
 };
