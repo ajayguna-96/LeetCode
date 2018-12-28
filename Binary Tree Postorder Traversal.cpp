b411b55d4c71a8045c8a173ebe576e2001f1484e
ajayguna-96 <ajayguna.96@gmail.com>
2018-12-27 19:17:48 -0700

    Update Binary Tree Postorder Traversal.cpp

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -1,13 +1,27 @@
-vector<int> postorderTraversal(TreeNode* root) {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
         stack<TreeNode*> stk;
         stk.push(root);
         while (!stk.empty()) {
             root = stk.top(); stk.pop();
             if (!root) continue;
-            ans.push_back(root->val);
             stk.push(root->left);
             stk.push(root->right);
+            ans.push_back(root->val);
+
         }
         return vector<int>(rbegin(ans), rend(ans));
     }
+
+};
